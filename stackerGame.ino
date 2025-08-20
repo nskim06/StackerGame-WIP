d1ae25de2d3d7bc0b5d9703f8a8d318903e72cf0
Noah Kim <nskim24.06@gmail.com>
2025-08-19 21:17:03 -0700

    stackerGame.ino

---
+++ stackerGame.ino
@@ -0,0 +1,35 @@
+// libraries/defines for 8x32 matrix
+#include <MD_Parola.h>
+#include <MD_MAX72xx.h>
+#include <SPI.h>
+
+#define HARDWARE_TYPE MD_MAX72XX::FC16_HW
+#define MAX_DEVICES 4
+#define CS_PIN 3
+MD_Parola myDisplay = MD_Parola(HARDWARE_TYPE, CS_PIN, MAX_DEVICES);
+
+// libraries for LCD display
+#include <LiquidCrystal_I2C.h>
+#include <Wire.h>
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+
+void setup() {
+  // put your setup code here, to run once:
+  lcd.init();
+  lcd.backlight();
+
+  myDisplay.begin();
+  myDisplay.setIntensity(0);
+  myDisplay.displayClear();
+}
+
+void loop() {
+  lcd.setCursor(0, 0);
+  lcd.print("Wassup");
+
+  myDisplay.setTextAlignment(PA_RIGHT);
+  myDisplay.print("Hello");
+
+  delay(3000);
+}
